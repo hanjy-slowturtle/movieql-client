a0bdd269d7c01b27074036a4edf8bc806f69d221
hanjy-slowturtle <h920915@gmail.com>
2022-10-20 23:33:14 +0900

    ~ #1.2 useQuery

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,25 +1,32 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      title
+    }
+    allTweets {
+      id
+      text
+      author {
+        fullName
+      }
+    }
+  }
+`;
 
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              title
-            }
-          }
-        `,
-      })
-      .then((results) => setMovies(results.data.allMovies));
-  }, [client]);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Error</h1>;
+  }
+  console.log(data);
   return (
     <ul>
-      {movies.map((movie, index) => (
+      {data.allMovies.map((movie, index) => (
         <li key={index}>{movie.title}</li>
       ))}
     </ul>
